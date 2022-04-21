376f33993f7e345f1259ba5bc0bcf2501dd2968d
valen <valentina.becerra@gmail.com>
2022-04-22 00:56:26 +0200

    feature ingenieering

--- functions.py
+++ functions.py
@@ -20,5 +20,5 @@ def plot_stock(df, Code, feature='Target', color='blue'):
     
 
 #create a dataframe for only one security code:
-def df_security_code(df, code):
+def df_security_code(df, code=8194):
 	return df.query('SecuritiesCode == @code')
