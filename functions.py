56ea1a4934000440310295daf190a0d21555b9b5
valen <valentina.becerra@gmail.com>
2022-04-25 10:45:31 +0200

    7203 Toyota

--- functions.py
+++ functions.py
@@ -37,7 +37,7 @@ def plot_stock(df, Code, feature='Target', color='blue'):
     
 
 #create a dataframe for only one security code:
-def df_security_code(df, code=8194):
+def df_security_code(df, code=7203): # Toyota
 	return df.query('SecuritiesCode == @code')
 
 
